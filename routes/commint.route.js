3d121c0265a7ead2084ea83a490ecd217d816759
Ahmed AL Awamleh <awamlehad@icloud.com>
2022-09-14 08:57:43 +0300

    solve backend

--- routes/commint.route.js
+++ routes/commint.route.js
@@ -4,7 +4,9 @@ const router = express.Router();
 const { Comment } = require('../models/index')
 
 router.get('/comment', getAllComment)
-router.post('/comment', crateComment);
+router.post('/comment/:id', addComment);
+router.put('/comment/:id', updateComment);
+router.delete('/comment/:id', deleteComment);
 
 
 async function getAllComment(req, res) {
@@ -12,10 +14,24 @@ async function getAllComment(req, res) {
     res.status(200).json({ comments })
 }
 
-async function crateComment(req, res) {
+async function addComment(req, res) {
     const newComment = req.body;
     const comment = await Comment.create(newComment)
     res.status(201).json(comment)
+}
+async function updateComment(req, res) {
+    const id = req.params.id;
+    const obj = req.body;
+    const comment = await Comment.update(id, obj);
+    res.status(201).json(comment);
+}
+async function deleteComment(req, res) {
+    const id = req.params.id
+    let deletedPost = await Comment.destroy({
+        where: { id: id }
+
+    })
+    res.status(204).json(deletedPost)
 }
 
 module.exports = router
